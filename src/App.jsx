d9f08182e2cc590d9fe9661f3aef658dff74f53b
Bordea Radu <radu.bordea@ha.ax>
2023-11-05 13:27:45 +0100

    create Touts and Tour components plus render them

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,48 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [isloading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isloading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  // TODO
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
+  );
 };
 export default App;
